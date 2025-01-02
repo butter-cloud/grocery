a27ba670c0f39f54fdba92a2b9ee96ecd8a64dd5
leejay210 <leejay@lgupluspartners.co.kr>
2025-01-02 11:58:31 +0900

    fix prettier config

--- app-front/eslint.config.mjs
+++ app-front/eslint.config.mjs
@@ -1,25 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     plugins: {
       prettier: eslintPluginPrettier,
     },
     rules: {
-      "prettier/prettier": "error",
+      'prettier/prettier': 'warn',
     },
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
