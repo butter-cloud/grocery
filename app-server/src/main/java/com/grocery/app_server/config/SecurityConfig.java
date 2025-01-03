160145bd5501abd9a09235160e5bbb50556dcd14
leejay210 <leejay@lgupluspartners.co.kr>
2025-01-03 18:24:53 +0900

    remove unused import

--- app-server/src/main/java/com/grocery/app_server/config/SecurityConfig.java
+++ app-server/src/main/java/com/grocery/app_server/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.grocery.app_server.config;
 
 import com.grocery.app_server.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
--- app-server/src/main/java/com/grocery/app_server/service/UserService.java
+++ app-server/src/main/java/com/grocery/app_server/service/UserService.java
@@ -5,7 +5,6 @@ import com.grocery.app_server.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
