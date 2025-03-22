44c32e5202dad9688da3e8f50f91eee100329655
leejay <leejay@gemvaxlink.com>
2025-03-23 01:53:37 +0900

    Add visual indicator for active menu item

--- app-front/src/ui/component/common/SideMenu.tsx
+++ app-front/src/ui/component/common/SideMenu.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import styled from 'styled-components'
 import { isLogin } from '@/util/CommonUtil'
 import api from '@/config/axiosInstance'
+import { usePathname } from 'next/navigation'
 
 const Menu = styled.div`
   position: absolute;
@@ -15,13 +18,15 @@ const Menu = styled.div`
   gap: 1rem;
   z-index: 99;
 `
-
-const MenuBar = styled.a`
+// $ means transient prop in react, it is used to pass props to styled components without adding them to the DOM
+// prevents the error of passing props to a DOM element that doesn't recognize them
+const MenuBar = styled.a<{ $isActive: boolean }>`
   text-align: center;
   font-size: 2rem;
   font-style: italic;
   font-weight: 500;
-  
+  color: ${({ $isActive, theme }) => ($isActive ? 'white' : 'black')};
+
   &:hover {
     color: ${({ theme }) => theme.colors.secondary};
   }
@@ -45,20 +50,27 @@ const Button = styled.button`
 `
 
 const SideMenu = () => {
+  const pathname = usePathname()
+
   const handleLogout = () => {
     localStorage.removeItem('accessToken')
     api.post('/auth/logout')
     window.location.href = '/'
   }
+
   return (
-    <>
-      <Menu>
-        <MenuBar href={'/'}>Home</MenuBar>
-        <MenuBar href={'/product/all'}>All Products</MenuBar>
-        <MenuBar href={'/product/best'}>BestSeller</MenuBar>
-        {isLogin() && <Button onClick={handleLogout}>Logout</Button>}
-      </Menu>
-    </>
+    <Menu>
+      <MenuBar href={'/'} $isActive={pathname === '/'}>
+        Home
+      </MenuBar>
+      <MenuBar href={'/product/all'} $isActive={pathname === '/product/all'}>
+        All Products
+      </MenuBar>
+      <MenuBar href={'/product/best'} $isActive={pathname === '/product/best'}>
+        BestSeller
+      </MenuBar>
+      {isLogin() && <Button onClick={handleLogout}>Logout</Button>}
+    </Menu>
   )
 }
 
