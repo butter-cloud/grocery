4ffc919fca8bee1ff8a81a310d22eeabbc60c8c8
leejay210 <leejay@ponylink.co.kr>
2025-03-26 11:25:30 +0900

    add productApi

---
+++ app-front/src/api/product/productApi.ts
@@ -0,0 +1,18 @@
+import api from '@/api/axiosInstance'
+
+class ProductApi {
+    async addNewProduct(product: TypeProduct) {
+        return await api.post('/product/new', product)
+    }
+
+    async getAllProducts() {
+        return await api.get('/product/all')
+    }
+
+    async getProductDetail(productId: number) {
+        return await api.get(`/product/${productId}`)
+    }
+}
+
+const productApi = new ProductApi()
+export default productApi
